7fc9b2b9d2e07c3907bb99f53d6025f5fe935705
editaahn <41330862+editaahn@users.noreply.github.com>
2020-12-09 21:16:50 +0900

    revise socket.js for different namespace

--- socket.js
+++ socket.js
@@ -1,25 +1,37 @@
-const SocketIO = require('socket.io')
+const SocketIO = require("socket.io");
 
 module.exports = (server) => {
   //웹소켓 서버 생성
   const io = SocketIO(server, { path: '/socket.io' });
 
-  // 클라이언트와 서버의 웹 소켓 연결 발생에 대한 event listener
-  io.on("connection", (socket) => { // 웹소켓 객체
+  app.set("io", io); // 라우터에서 io 객체를 쓸 수 있게 저장. req.app.get('io')로 접근
+
+  // of() : socket.io에 네임스페이스 부여
+  // Socket.IO는 기본적으로 / 네임스페이스에 접속. => of 메서드를 사용하면 다른 네임스페이스를 만들어 접속.
+  const room = io.of("/room"); //
+  const chat = io.of("/chat");
+
+  //  /room 네임스페이스에 연결된 클라이언트에게만 데이터를 보냄
+  room.on("connection", (socket) => { 
+    console.log("room 네임스페이스에 접속");
+    socket.on("disconnect", () => {
+      console.log("room 네임스페이스 접속 해제");
+    });
+  });
+
+  chat.on("connection", (socket) => { // 웹소켓 객체
+    console.log("chat 네임스페이스에 접속");
     const req = socket.request; // 요청 객체에 접근
-    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress; // client ip를 알아냄
-
-    console.log("새로운 클라이언트 접속", ip, socket.id, req.ip); // socket.id로 소켓의 주인 식별
-
-    socket.on("error", (error) => console.error(error)); // error: 웹 소켓 연결 중 문제 발생한 경우
-    socket.on('disconnect', () => { // close: client와 연결이 끊긴 경우
-      console.log('클라이언트 접속 해제', ip, socket.id);
-      clearInterval(socket.interval);
-    })
-    socket.on("reply", (data) => console.log(data)); // reply: 사용자 정의 이벤트. 클라이언트에서 reply라는 이벤트명으로 데이터를 보내는 지 listen
-
-    socket.interval = setInterval(() => { // 모든 클라이언트에게 메시지 전송
-      socket.emit('news', 'Hello Socket.IO') // 첫번째 인자는 이벤트이름, 두번째는 전달할 데이터
-    }, 3000)
+    const {
+      headers: { referer },
+    } = req;
+    const roomId = referer
+      .split("/")[referer.split("/").length - 1]
+      .replace(/\?.+/, "");
+    socket.join(roomId); // 접속 시 join메서드. 방 정보를 인자로 받음.
+    socket.on("disconnect", () => { // 접속 후 disconnect 이벤트 리스너 등록
+      console.log("chat 네임스페이스 접속 해제");
+      socket.leave(roomId);
+    });
   });
 };
