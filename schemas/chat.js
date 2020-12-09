836489eaa568c1484213a5d82300394cf9ebe48e
editaahn <41330862+editaahn@users.noreply.github.com>
2020-12-09 19:55:06 +0900

    connection to MongoDB (Mongogoose)

---
+++ schemas/chat.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+const {
+  Types: { ObjectId },
+} = Schema;
+
+const chatSchema = new Schema({
+  room: { // 채팅방 아이디
+    type: ObjectId,
+    required: true,
+    ref: "Room",
+  },
+  user: { // 채팅한 유저
+    type: String,
+    required: true,
+  },
+  chat: String, // 채팅 내역
+  gif: String, // gif 주소
+  createdAt: { // 채팅 시간
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Chat', chatSchema);
