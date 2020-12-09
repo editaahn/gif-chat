bd56a988c00cc0ca81e571e6fd9c87fec1c46c22
editaahn <41330862+editaahn@users.noreply.github.com>
2020-12-10 00:14:27 +0900

    render main

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 
+const Room = require("../schemas/room");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("index");
+router.get("/", async (req, res, next) => {
+  // 메인 화면 렌더링. 채팅방 목록 보여줌
+  try {
+    const rooms = await Room.find({});
+    res.render("main", {
+      rooms,
+      title: "GIF 채팅방",
+      error: req.flash("roomError"),
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 module.exports = router;
